ebf4763d771cc9388c3a86fa63a56dffa0955589
Edinei <galera.org@gmail.com>
2018-12-13 16:49:54 -0200

    route with dashboard

--- public/src/Config/Config.php
+++ public/src/Config/Config.php
@@ -26,6 +26,8 @@ class Config
     {
         $rotas = json_decode(file_get_contents(PATH_HOME . "_config/route.json"), true);
 
+        unset($rotas['dashboard'], $rotas['entity-ui'], $rotas['dev-ui']);
+
         /* Por hora, não cria cache de conteúdo do painel administrativo */
         /*if(!empty($_SESSION['userlogin']) && $_SESSION['userlogin']['setor'] > 0) {
             $rotas[] = "dashboard";
