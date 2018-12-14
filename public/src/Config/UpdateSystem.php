008306ce85b49d73d297406c497cc16bf63e3c41
Edinei <galera.org@gmail.com>
2018-12-14 11:56:29 -0200

    icone baseado nas configurações

--- public/src/Config/UpdateSystem.php
+++ public/src/Config/UpdateSystem.php
@@ -358,11 +358,12 @@ class UpdateSystem
      */
     private function createCoreImages()
     {
+        $config = json_decode(file_get_contents(PATH_HOME . "_config/config.json"), true);
         copy(PATH_HOME . VENDOR . "config/public/assets/dino.png", PATH_HOME . "assetsPublic/img/dino.png");
         copy(PATH_HOME . VENDOR . "config/public/assets/save.gif", PATH_HOME . "assetsPublic/img/save.gif");
         copy(PATH_HOME . VENDOR . "config/public/assets/image-not-found.png", PATH_HOME . "assetsPublic/img/img.png");
-        copy(PATH_HOME . FAVICON, PATH_HOME . "assetsPublic/img/favicon.png");
-        copy((!empty(LOGO) ? PATH_HOME . LOGO : PATH_HOME . VENDOR . "config/public/assets/image-not-found.png"), PATH_HOME . "assetsPublic/img/logo.png");
+        copy(PATH_HOME . $config['favicon'], PATH_HOME . "assetsPublic/img/favicon.png");
+        copy((!empty($config['logo']) ? PATH_HOME . $config['logo'] : PATH_HOME . VENDOR . "config/public/assets/image-not-found.png"), PATH_HOME . "assetsPublic/img/logo.png");
     }
 
     /**
