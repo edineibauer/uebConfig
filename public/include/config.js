663b95845cad76741b07229e0ae5b97a6abc3a4c
Edinei <galera.org@gmail.com>
2018-12-14 08:57:52 -0200

    clear caches in new project

--- public/include/config.js
+++ public/include/config.js
@@ -1,3 +1,22 @@
+
+//delete service workers
+navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    for (let registration of registrations) {
+        registration.unregister()
+    }
+});
+
+//delete cookies
+var cookies = document.cookie.split(";");
+
+for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i];
+    var eqPos = cookie.indexOf("=");
+    var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+}
+
+//delete caches
 caches.keys().then(cacheNames => {
     return Promise.all(cacheNames.map(cacheName => {
         return caches.delete(cacheName)
