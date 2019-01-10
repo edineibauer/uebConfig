4c9c9d3fcad57d0cc2a0a61c66058c05407c0637
Edinei <galera.org@gmail.com>
2019-01-10 17:49:33 -0200

    appdata with permission check

--- public/get/appData.php
+++ public/get/appData.php
@@ -1,13 +1,20 @@
 <?php
 
+$setor = empty($_SESSION['userlogin']['setor']) ? 0 : $_SESSION['userlogin']['setor'];
 $read = new \Conn\Read();
-$entidadesAllow = \Config\Config::getPermissoes((int) $_SESSION['userlogin']['setor']);
+$entidadesAllow = \Config\Config::getPermission();
 $data['data'] = [];
 
 foreach (\Helpers\Helper::listFolder(PATH_HOME . "entity/cache") as $entityFile) {
     $entity = str_replace('.json', '', $entityFile);
-    if(preg_match('/.json$/i', $entityFile) && in_array($entity, $entidadesAllow['read'])){
-        $read->exeRead($entity, "ORDER BY id DESC");
+    if(preg_match('/.json$/i', $entityFile) && $entidadesAllow[$setor][$entity]['read']){
+        //Verifica se é multitenancy, se for, adiciona cláusula para buscar somente os dados referentes ao usuário
+        $info = \Entity\Metadados::getInfo($entity);
+        $where = null;
+        if(!empty($info['autor']) && $info['autor'] === 2)
+            $where = "WHERE ownerpub = " . $_SESSION['userlogin']['id'] . " ";
+
+        $read->exeRead($entity, $where);
         $data['data'][$entity] = $read->getResult();
     }
 }
