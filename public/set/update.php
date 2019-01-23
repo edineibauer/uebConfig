667e747390a1180df75d2b02c94ac02ae3e065c3
Edinei <galera.org@gmail.com>
2019-01-23 16:06:23 -0200

    update set new version

--- public/set/update.php
+++ public/set/update.php
@@ -1,3 +1,5 @@
 <?php
+if(filter_input(INPUT_POST, 'update', FILTER_VALIDATE_BOOLEAN))
+    \Config\Config::updateSite();
 
 $data['data'] = file_get_contents(PATH_HOME . '_config/updates/update.txt');
