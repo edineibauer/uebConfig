db7148decc7462940c1d71a0666acd925a1ce68b
Edinei <galera.org@gmail.com>
2019-01-28 18:24:41 -0200

    ajustes get dicionario

--- public/get/dicionarios.php
+++ public/get/dicionarios.php
@@ -1,5 +1,5 @@
 <?php
-$setor = empty($_SESSION['userlogin']['setor']) ? 0 : (int) $_SESSION['userlogin']['setor'];
+$setor = empty($_SESSION['userlogin']['setor']) ? 0 : (int)$_SESSION['userlogin']['setor'];
 $entityNot = \Config\Config::getEntityNotAllow()[$setor];
 
 //convert dicionário para referenciar colunas e não ids
@@ -8,14 +8,17 @@ $dicionarioOrdenado = [];
 foreach (Entity\Entity::dicionario() as $entity => $metas) {
     $indice = 99999;
     foreach ($metas as $i => $meta) {
-        if($meta['key'] !== "identifier" && !in_array($entity, $entityNot)) {
+        if ($meta['key'] !== "identifier" && !in_array($entity, $entityNot)) {
             $meta['id'] = $i;
             $dicionario[$entity][$meta['indice'] ?? $indice++] = $meta;
         }
     }
-    ksort($dicionario[$entity]);
-    foreach ($dicionario[$entity] as $i => $meta)
-        $dicionarioOrdenado[$entity][$meta['column']] = $meta;
+
+    if (!empty($dicionario[$entity])) {
+        ksort($dicionario[$entity]);
+        foreach ($dicionario[$entity] as $i => $meta)
+            $dicionarioOrdenado[$entity][$meta['column']] = $meta;
+    }
 }
 
 $data['data'] = $dicionarioOrdenado;
