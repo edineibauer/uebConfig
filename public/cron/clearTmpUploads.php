4b5f6db7cb9b4a1d10003e347119d766e1096cf4
Edinei J. Bauer <galera.org@gmail.com>
2019-03-29 16:46:43 -0300

    mantém os uploads temporários por 1 dia

---
+++ public/cron/clearTmpUploads.php
@@ -0,0 +1,9 @@
+<?php
+
+foreach (Helpers\Helpers::listFolder(PATH_HOME . "uploads/tmp") as $item) {
+    $time = strtotime('+1 day', filemtime(PATH_HOME . "uploads/tmp/{$item}"));
+    $hoje = strtotime('now');
+
+    if($time < $hoje)
+        unlink(PATH_HOME . "uploads/tmp/{$item}");
+}
