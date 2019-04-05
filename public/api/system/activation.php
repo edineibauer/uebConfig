cf6c3b92ca645470dcc47bce6ab31d24c4c19f35
Edinei J. Bauer <galera.org@gmail.com>
2019-04-05 18:01:30 -0300

    update activation response

--- public/api/system/activation.php
+++ public/api/system/activation.php
@@ -1,18 +1,21 @@
 <?php
+
+use \Config\Config;
+
 if(!defined("KEY") && !empty($key)) {
 
     //Adiciona constante KEY na config
     $config = json_decode(file_get_contents(PATH_HOME . "_config/config.json"), true);
     $config['key'] = $key;
-    \Config\Config::createConfig($config);
+    Config::createConfig($config);
 
     //retorna versão das bibliotecas e força o uso de versões fixas
     $comp = json_decode(file_get_contents(PATH_HOME . "composer.json"), true);
     $teste = json_decode(file_get_contents(PATH_HOME . "composer.lock"), true);
-    $libs = [];
+    $data['data'] = [];
     foreach ($teste['packages'] as $package) {
         if (preg_match('/^ueb\//i', $package['name'])){
-            $libs[] = ["versao" => $package['version'], "nome" => $package['name']];
+            $data['data'][] = ["versao" => $package['version'], "biblioteca" => $package['name']];
 
             //adiciona dependencias para o composer.json
             $comp['require'][$package['name']] = $package['version'];
@@ -25,8 +28,6 @@ if(!defined("KEY") && !empty($key)) {
     $f = fopen(PATH_HOME . "composer.json", "w+");
     fwrite($f, str_replace('\/', '/', json_encode($comp)));
     fclose($f);
-
-    $data['data'] = json_encode($libs);
 } else {
     $data['error'] = "chave já definida ou não informada";
 }
