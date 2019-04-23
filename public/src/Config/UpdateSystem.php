0b323c079a3caef46f20bdc9f818ed1bb47ad74f
Edinei J. Bauer <galera.org@gmail.com>
2019-04-22 22:15:44 -0300

    file exist

--- public/src/Config/UpdateSystem.php
+++ public/src/Config/UpdateSystem.php
@@ -401,9 +401,11 @@ class UpdateSystem
         copy(PATH_HOME . VENDOR . "config/public/assets/save.gif", PATH_HOME . "assetsPublic/img/save.gif");
         copy(PATH_HOME . VENDOR . "config/public/assets/file_type.svg", PATH_HOME . "assetsPublic/img/file_type.svg");
         copy(PATH_HOME . VENDOR . "config/public/assets/image-not-found.png", PATH_HOME . "assetsPublic/img/img.png");
+
+        if(file_exists(PATH_HOME . (!empty($config['favicon']) ? "uploads/site/favicon.png" : VENDOR . "config/public/assets/favicon.png")))
         copy(PATH_HOME . (!empty($config['favicon']) ? "uploads/site/favicon.png" : VENDOR . "config/public/assets/favicon.png"), PATH_HOME . "assetsPublic/img/favicon.png");
 
-        if(!empty($config['logo']))
+        if(!empty($config['logo']) && file_exists(PATH_HOME . "uploads/site/logo.png"))
             copy(PATH_HOME . "uploads/site/logo.png", PATH_HOME . "assetsPublic/img/logo.png");
 
         elseif(file_exists(PATH_HOME . "assetsPublic/img/logo.png"))
