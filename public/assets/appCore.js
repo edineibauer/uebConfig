7be13f16f2ca12a7ee122bf9d30432e9b296b908
Edinei J. Bauer <galera.org@gmail.com>
2019-07-08 15:40:33 -0300

    setcookie to 1 year if not informed

--- public/assets/appCore.js
+++ public/assets/appCore.js
@@ -49,6 +49,7 @@ function get(file) {
 
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
+    exdays = typeof exdays === "undefined" ? 360 : exdays;
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/"
