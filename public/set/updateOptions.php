705ab5c6a08e942e89683995a20c379b9f3ed18d
Edinei J. Bauer <galera.org@gmail.com>
2019-07-25 21:25:08 -0300

    not update autosync

--- public/set/updateOptions.php
+++ public/set/updateOptions.php
@@ -2,7 +2,6 @@
 $conf = json_decode(file_get_contents(PATH_HOME . "_config/config.json"), true);
 $conf['homepage'] = filter_input(INPUT_POST, 'homepage', FILTER_VALIDATE_BOOLEAN) ? 1 : 0;
 $conf['limitoffline'] = filter_input(INPUT_POST, 'limitoffline', FILTER_DEFAULT);
-$conf['autosync'] = filter_input(INPUT_POST, 'autosync', FILTER_VALIDATE_BOOLEAN) ? 1 : 0;
 
 \Config\Config::createConfig($conf);
 
