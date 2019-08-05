964805bce8cc07ee1c0f5fccc161dbf172fb64a5
Edinei J. Bauer <galera.org@gmail.com>
2019-08-05 09:55:27 -0300

    função de download

--- public/assets/appCore.js
+++ public/assets/appCore.js
@@ -124,6 +124,17 @@ function view(file, funcao) {
     });
 }
 
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.setAttribute('target', '_blank');
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element)
+}
+
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     exdays = typeof exdays === "undefined" ? 360 : exdays;
