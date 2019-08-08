8fb14380faf32bfa4bb1438684ed1de3c090f8ac
Edinei J. Bauer <galera.org@gmail.com>
2019-08-07 21:07:49 -0300

    json default value accept string and integer

--- public/assets/indexedDB.js
+++ public/assets/indexedDB.js
@@ -605,8 +605,10 @@ function getDefaultValue(meta, value) {
     let valor = "";
     if (typeof meta === "object" && meta !== null) {
         if ((['boolean', 'status'].indexOf(meta.format) === -1 && value === !1) || value === null)
-            value = ""; else if (meta.type === "json")
-            value = typeof value === "object" ? value : (isJson(value) ? JSON.parse(value) : ""); else if (meta.group === "date")
+            value = "";
+        else if (meta.type === "json")
+            value = typeof value === "object" ? value : (isJson(value) ? JSON.parse(value) : ((typeof value === 'number' || typeof value === 'string') && value !== "" ? "[" + value + "]" : ""));
+        else if (meta.group === "date")
             value = value.replace(" ", "T");
         switch (meta.format) {
             case 'boolean':
