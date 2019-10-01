5cb601f5694954ed67e5058949216095c82fd0d9
Edinei J. Bauer <galera.org@gmail.com>
2019-09-30 23:41:02 -0300

    css transition header fixed

--- public/assets/appCore.js
+++ public/assets/appCore.js
@@ -903,7 +903,7 @@ function defaultPageTransitionPosition(direction, $element) {
     let left = $element[0].getBoundingClientRect().left;
     let topHeader = $("#core-header").css("opacity") !== "0" ? $("#core-header")[0].clientHeight : 0;
     $element.css({
-        "min-height": (window.innerHeight - topHeader - (window.innerWidth < 900 && $("#core-header-nav-bottom").hasClass("s-show") ? 50 : 0)) + "px",
+        "min-height": (window.innerHeight - topHeader - (window.innerWidth < 900 && $("#core-header-nav-bottom").css("opacity") === "1" && $("#core-header-nav-bottom").hasClass("s-show") ? 50 : 0)) + "px",
         "position": "fixed",
         "top": $element[0].getBoundingClientRect().top + "px",
         "width": $element[0].clientWidth + "px",
@@ -1145,7 +1145,7 @@ var app = {
                     else
                         $("#core-header-nav-bottom").removeClass("core-show-header-nav");
 
-                    let topHeader = g.header && $("#core-header").css("opacity") !== "0" ? $("#core-header")[0].clientHeight : 0;
+                    let topHeader = g.header ? $("#core-header")[0].clientHeight : 0;
                     $div.css("min-height", (window.innerHeight - topHeader - (window.innerWidth < 900 && g.navbar && $("#core-header-nav-bottom").hasClass("s-show") ? 50 : 0)) + "px");
 
                     if (g.js.length) {
