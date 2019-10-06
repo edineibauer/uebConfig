a7fe27a06db500d6c246e9cea4a0737ad1fbb623
Edinei J. Bauer <galera.org@gmail.com>
2019-10-06 00:42:51 -0300

    assets from public folder too

--- public/src/Config/UpdateSystem.php
+++ public/src/Config/UpdateSystem.php
@@ -329,6 +329,26 @@ class UpdateSystem
             Helper::createFolderIfNoExist(PATH_HOME . "assetsPublic");
             $minifier = new Minify\JS("");
 
+            if(is_array($jsList) && !empty($jsList)) {
+                foreach ($jsList as $i => $js) {
+                    if(file_exists(PATH_HOME . "public/assets/{$js}.js")) {
+                        $minifier->add(PATH_HOME . "public/assets/{$js}.js");
+                        unset($jsList[$i]);
+                    } elseif(file_exists(PATH_HOME . "public/assets/js/{$js}.js")) {
+                        $minifier->add(PATH_HOME . "public/assets/js/{$js}.js");
+                        unset($jsList[$i]);
+                    }
+                }
+            } elseif(is_string($jsList)) {
+                if(file_exists(PATH_HOME . "public/assets/{$jsList}.js")) {
+                    $minifier->add(PATH_HOME . "public/assets/{$jsList}.js");
+                    $jsList = [];
+                } elseif(file_exists(PATH_HOME . "public/assets/js/{$jsList}.js")) {
+                    $minifier->add(PATH_HOME . "public/assets/js/{$jsList}.js");
+                    $jsList = [];
+                }
+            }
+
             foreach ($data as $datum) {
                 if (in_array($datum['nome'], $jsList)) {
                     foreach ($datum['arquivos'] as $file) {
@@ -353,6 +373,26 @@ class UpdateSystem
             Helper::createFolderIfNoExist(PATH_HOME . "assetsPublic");
 
             $minifier = new Minify\CSS("");
+
+            if(is_array($cssList) && !empty($cssList)) {
+                foreach ($cssList as $i => $css) {
+                    if(file_exists(PATH_HOME . "public/assets/{$css}.css")) {
+                        $minifier->add(PATH_HOME . "public/assets/{$css}.css");
+                        unset($cssList[$i]);
+                    } elseif(file_exists(PATH_HOME . "public/assets/css/{$css}.css")) {
+                        $minifier->add(PATH_HOME . "public/assets/css/{$css}.css");
+                        unset($cssList[$i]);
+                    }
+                }
+            } elseif(is_string($cssList)) {
+                if(file_exists(PATH_HOME . "public/assets/{$cssList}.css")) {
+                    $minifier->add(PATH_HOME . "public/assets/{$cssList}.css");
+                    $cssList = [];
+                } elseif(file_exists(PATH_HOME . "public/assets/css/{$cssList}.css")) {
+                    $minifier->add(PATH_HOME . "public/assets/css/{$cssList}.css");
+                    $cssList = [];
+                }
+            }
 
             foreach ($cssList as $item) {
                 $datum = array_values(array_filter(array_map(function ($d) use ($item) {
@@ -418,7 +458,7 @@ class UpdateSystem
         copy(PATH_HOME . VENDOR . "config/public/assets/file_type.svg", PATH_HOME . "assetsPublic/img/file_type.svg");
 
         if(file_exists(PATH_HOME . (!empty($config['favicon']) ? $config['favicon'] : VENDOR . "config/public/assets/favicon.png")))
-        copy(PATH_HOME . (!empty($config['favicon']) ? $config['favicon'] : VENDOR . "config/public/assets/favicon.png"), PATH_HOME . "assetsPublic/img/favicon.png");
+            copy(PATH_HOME . (!empty($config['favicon']) ? $config['favicon'] : VENDOR . "config/public/assets/favicon.png"), PATH_HOME . "assetsPublic/img/favicon.png");
 
         if(!empty($config['logo']) && file_exists(PATH_HOME . $config['logo']))
             copy(PATH_HOME . $config['logo'], PATH_HOME . "assetsPublic/img/logo.png");
