3dbf00f8232e9f8f83554a5a7dd8cc7da3a399a6
Edinei J. Bauer <galera.org@gmail.com>
2019-10-30 08:42:07 -0300

    accept DEV option on DEVUI

--- public/set/updateOptions.php
+++ public/set/updateOptions.php
@@ -2,6 +2,7 @@
 $conf = json_decode(file_get_contents(PATH_HOME . "_config/config.json"), true);
 $conf['homepage'] = filter_input(INPUT_POST, 'homepage', FILTER_VALIDATE_BOOLEAN) ? 1 : 0;
 $conf['serviceworker'] = filter_input(INPUT_POST, 'serviceworker', FILTER_VALIDATE_BOOLEAN) ? 1 : 0;
+$conf['dev'] = filter_input(INPUT_POST, 'dev', FILTER_VALIDATE_BOOLEAN) ? 1 : 0;
 $conf['limitoffline'] = filter_input(INPUT_POST, 'limitoffline', FILTER_DEFAULT);
 
 \Config\Config::createConfig($conf);
