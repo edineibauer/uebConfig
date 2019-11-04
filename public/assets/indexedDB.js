ff7c6ed507fbeb7165ffd9130b25cbe603056663
Edinei J. Bauer <galera.org@gmail.com>
2019-11-04 20:14:26 -0300

    accept imagem as string or object im sidebar

--- public/assets/indexedDB.js
+++ public/assets/indexedDB.js
@@ -146,12 +146,12 @@ function exeRead(entity, filter, order, reverse, limit, offset) {
                                 }
                             });
                         } else {
-                            toast("Sem Conexão! Conecte para ver todos os registros", 7000, "toast-warning");
+                            toast("Sem Conexão! Conecte para atualizar os registros", 7000, "toast-warning");
                             resolve(readOffline(data, filter, order, reverse, limit, offset));
                         }
                     },
                     error: function (e) {
-                        toast("Sem Conexão! Conecte para ver todos os registros", 7000, "toast-warning");
+                        toast("Sem Conexão! Conecte para atualizar os registros", 7000, "toast-warning");
                         resolve(readOffline(data, filter, order, reverse, limit, offset));
                     },
                     dataType: "json",
