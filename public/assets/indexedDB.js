cb2b6e9f2a5f779ad8cde2a58b9198e71db9e8e6
Edinei J. Bauer <galera.org@gmail.com>
2019-11-25 16:12:36 -0300

    limit minimo da grid

--- public/assets/indexedDB.js
+++ public/assets/indexedDB.js
@@ -88,6 +88,7 @@ function exeRead(entity, filter, order, reverse, limit, offset) {
     reverse = (typeof reverse !== "undefined" ? (reverse ? !0 : !1) : !1);
     limit = parseInt(typeof limit === "number" ? limit : (localStorage.limitGrid ? localStorage.limitGrid : 15));
     limit = limit > parseInt(LIMITOFFLINE) ? parseInt(LIMITOFFLINE) : limit;
+    limit = limit < parseInt(localStorage.limitGrid) ? parseInt(localStorage.limitGrid) : limit;
     offset = parseInt((typeof offset === "number" ? offset : 0) - 1);
 
     return db.exeRead(entity).then(data => {
