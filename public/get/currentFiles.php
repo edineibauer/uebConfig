7a13570e8b5f8945bd09f6432b6dcebed815688d
Edinei J. Bauer <galera.org@gmail.com>
2020-01-15 21:38:52 -0300

    adiciona o core em assetsPublic para o cache service worker

--- public/get/currentFiles.php
+++ public/get/currentFiles.php
@@ -78,6 +78,13 @@ foreach (Helper::listFolder(PATH_HOME . "assetsPublic") as $item) {
         $data['data']['core'][] = HOME . "assetsPublic/{$item}?v=" . VERSION;
 }
 
+//CORE, create cache from all 'assetsPublic' root and current view
+$setor = empty($_SESSION['userlogin']) ? "0" : $_SESSION['userlogin']['setor'];
+foreach (Helper::listFolder(PATH_HOME . "assetsPublic/core/" . $setor) as $item) {
+    if (!is_dir(PATH_HOME . "assetsPublic/{$item}") && strpos($item, ".") && !in_array(HOME . "assetsPublic/{$item}", $data['data']['core']))
+        $data['data']['core'][] = HOME . "assetsPublic/core/{$setor}/{$item}?v=" . VERSION;
+}
+
 //fonts
 foreach (Helper::listFolder(PATH_HOME . "assetsPublic/fonts") as $iten)
     $data['data']['fonts'][] = HOME . "assetsPublic/fonts/{$iten}?v=" . VERSION;
