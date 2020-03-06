22d34a660005fd53ce454a4eae3acf27be898553
Edinei J. Bauer <galera.org@gmail.com>
2020-03-05 22:46:41 -0300

    on resize, get new page height min

--- public/assets/appCore.js
+++ public/assets/appCore.js
@@ -1491,6 +1491,11 @@ if (SERVICEWORKER) {
     });
 }
 
+function getPageHeight() {
+    let topHeader = g.header ? $("#core-header")[0].clientHeight : 0;
+    return (window.innerHeight - topHeader - (window.innerWidth < 900 && g.navbar && $("#core-header-nav-bottom").hasClass("s-show") ? 50 : 0));
+}
+
 var dicionarios;
 var swRegistration = null;
 var aniTransitionPage = null;
@@ -1560,8 +1565,7 @@ var app = {
                     else
                         $("#core-header-nav-bottom").removeClass("core-show-header-navbar");
 
-                    let topHeader = g.header ? $("#core-header")[0].clientHeight : 0;
-                    let minHeightContent = ($div.attr("id") === "core-content" || typeof $div.attr("id") === "undefined" ? (window.innerHeight - topHeader - (window.innerWidth < 900 && g.navbar && $("#core-header-nav-bottom").hasClass("s-show") ? 50 : 0)) : 0);
+                    let minHeightContent = ($div.attr("id") === "core-content" || typeof $div.attr("id") === "undefined" ? getPageHeight() : 0);
                     $div.css("min-height", minHeightContent + "px");
                     if(file === "dashboard")
                         $(".dashboard-main, #dashboard").css("min-height", minHeightContent + "px");
@@ -1922,6 +1926,7 @@ function onLoadDocument() {
 
     window.onresize = function () {
         clearHeaderScrollPosition();
+        $("#core-content, .dashboard-main, #dashboard").css("min-height", getPageHeight() + "px");
 
         if (window.innerWidth < 994)
             updateHeaderPosition();
