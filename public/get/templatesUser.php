8c9fbf1da4811fbfc155dfd90b0cdcaf64eeae09
Edinei J. Bauer <galera.org@gmail.com>
2020-03-22 11:30:53 -0300

    templates user fixed

--- public/get/templatesUser.php
+++ public/get/templatesUser.php
@@ -11,6 +11,46 @@ function getTemplates(string $lib, string $setor, array $list)
     /**
      * Se existir templates nesta lib
      */
+    if (file_exists(PATH_HOME . VENDOR . $lib . "/public/tpl")) {
+
+        /**
+         * Para cada template dentro desta lib
+         */
+        foreach (\Helpers\Helper::listFolder(PATH_HOME . VENDOR . $lib . "/public/tpl") as $tpl) {
+            if (preg_match("/\.(mst|mustache)$/i", $tpl)) {
+                $nameTpl = str_replace(['.mst', '.mustache'], '', $tpl);
+
+                /**
+                 * Se este template ainda não foi adicionado na lista, então adiciona
+                 */
+                if (!in_array($nameTpl, array_keys($list))) {
+
+                    /**
+                     * busca overload do template atual da lib em PUBLIC
+                     */
+                    if (file_exists(PATH_HOME . "public/overload/{$lib}/tpl/{$setor}/{$nameTpl}.mustache")) {
+                        $list[$nameTpl] = file_get_contents(PATH_HOME . "public/overload/{$lib}/tpl/{$setor}/{$nameTpl}.mustache");
+
+                    } else {
+
+                        /**
+                         * Busta overload do template atual da lib em outras libs
+                         */
+                        foreach (\Helpers\Helper::listFolder(PATH_HOME . VENDOR) as $libOverload) {
+                            if ($libOverload !== $lib) {
+                                if (file_exists(PATH_HOME . VENDOR . $libOverload . "/public/overload/{$lib}/tpl/{$setor}/{$nameTpl}.mustache"))
+                                    $list[$nameTpl] = file_get_contents(PATH_HOME . VENDOR . $libOverload . "/public/overload/{$lib}/tpl/{$setor}/{$nameTpl}.mustache");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Se existir templates nesta lib
+     */
     if (file_exists(PATH_HOME . VENDOR . $lib . "/public/tpl/{$setor}")) {
 
         /**
