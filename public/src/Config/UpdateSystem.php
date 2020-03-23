9e7640d3d51c0f12e3237eb90791573018b64067
Edinei J. Bauer <galera.org@gmail.com>
2020-03-22 22:44:52 -0300

    change position functions

--- public/src/Config/UpdateSystem.php
+++ public/src/Config/UpdateSystem.php
@@ -60,14 +60,12 @@ class UpdateSystem
                 fwrite($f, file_get_contents(PATH_HOME . "composer.lock"));
                 fclose($f);
 
-                $this->updateVersionNumber();
                 $this->updateVersion($custom);
 
             } elseif (file_exists(PATH_HOME . "_config/updates/version.txt")) {
                 $keyVersion = file_get_contents(PATH_HOME . "composer.lock");
                 $old = file_get_contents(PATH_HOME . "_config/updates/version.txt");
                 if (!empty($custom) || $old !== $keyVersion) {
-                    $this->updateVersionNumber();
                     $this->updateVersion($custom);
                 }
             }
@@ -123,6 +121,7 @@ class UpdateSystem
      */
     private function updateVersion(array $custom)
     {
+        $this->updateVersionNumber();
         //cria/atualiza update log file
         Config::updateSite();
         Config::createLibsDirectory();
