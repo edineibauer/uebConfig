e07cd6c7b3022480f7221c4f65a45a40f36550a2
Edinei J. Bauer <galera.org@gmail.com>
2020-03-26 00:59:18 -0300

    fix js

--- public/get/appFilesViewUser.php
+++ public/get/appFilesViewUser.php
@@ -9,5 +9,5 @@ $setor = !empty($_SESSION['userlogin']) ? $_SESSION['userlogin']['setor'] : "0";
 $views = (file_exists(PATH_HOME . "_config/offline/{$setor}/view.json") ? json_decode(file_get_contents(PATH_HOME . "_config/offline/{$setor}/view.json"), !0) : []);
 foreach ($views as $view) {
     $data['data']['view'][] = "view/" . $view;
-    $data['data']['js'][] = (file_exists(PATH_HOME . "assetsPublic/view/{$setor}/{$view}.min.js?v=" . VERSION) ? "assetsPublic/view/{$setor}/{$view}.min.js" : "assetsPublic/view/{$view}.min.js?v=" . VERSION);
+    $data['data']['js'][] = (file_exists(PATH_HOME . "assetsPublic/view/{$setor}/{$view}.min.js") ? "assetsPublic/view/{$setor}/{$view}.min.js?v=" . VERSION : "assetsPublic/view/{$view}.min.js?v=" . VERSION);
 }
