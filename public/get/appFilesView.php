e4673b7ef87a4d503c911d4f2fc373d7b7720cd7
Edinei J. Bauer <galera.org@gmail.com>
2020-03-26 00:04:16 -0300

    viewUser return list object with view and js

--- public/get/appFilesView.php
+++ public/get/appFilesView.php
@@ -13,32 +13,3 @@ $data['data'] = ["view" => []];
  * View Offline
  */
 $data['data']['view'] = (file_exists(PATH_HOME . "_config/offline/view.json") ? json_decode(file_get_contents(PATH_HOME . "_config/offline/view.json"), !0) : []);
-
-/*if (file_exists(PATH_HOME . "_config/offline/view.json")) {
-    foreach (json_decode(file_get_contents(PATH_HOME . "_config/offline/view.json"), !0) as $item) {
-
-        $findItem = !1;
-        if (file_exists(PATH_HOME . "public/view/{$item}.php") || file_exists(PATH_HOME . "public/view/{$item}.html")) {
-            $findItem = !0;
-
-        } else {
-
-            foreach (Helper::listFolder(PATH_HOME . VENDOR) as $lib) {
-
-                foreach (Helper::listFolder(PATH_HOME . VENDOR) as $libOverload) {
-                    if ($libOverload !== $lib) {
-                        if (file_exists(PATH_HOME . VENDOR . $libOverload . "/public/overload/{$lib}/view/{$item}.php") || file_exists(PATH_HOME . VENDOR . $libOverload . "/public/overload/{$lib}/view/{$item}.html")) {
-                            $findItem = !0;
-                            break;
-                        }
-                    }
-                }
-
-                $findItem = (!$findItem && file_exists(PATH_HOME . VENDOR . $lib . "/public/view/{$item}.php") || file_exists(PATH_HOME . VENDOR . $lib . "/public/view/{$item}.html"));
-            }
-        }
-
-        if($findItem)
-            $data['data']['view'][] = HOME . "view/" . str_replace(['.php', '.html'], '', $item);
-    }
-}*/
--- public/get/appFilesViewUser.php
+++ public/get/appFilesViewUser.php
@@ -13,37 +13,6 @@ $setor = !empty($_SESSION['userlogin']) ? $_SESSION['userlogin']['setor'] : "0";
 /**
  * View Offline
  */
-$data['data']['view'] = (file_exists(PATH_HOME . "_config/offline/{$setor}/view.json") ? json_decode(file_get_contents(PATH_HOME . "_config/offline/{$setor}/view.json"), !0) : []);
-
-/*
-if (file_exists(PATH_HOME . "_config/offline/{$setor}/view.json")) {
-    foreach (json_decode(file_get_contents(PATH_HOME . "_config/offline/{$setor}/view.json"), !0) as $item) {
-
-        $find = !1;
-        if (file_exists(PATH_HOME . "public/view/{$setor}/{$item}.php") || file_exists(PATH_HOME . "public/view/{$setor}/{$item}.html") || file_exists(PATH_HOME . "public/view/{$item}.php") || file_exists(PATH_HOME . "public/view/{$item}.html")) {
-
-            $data['data']['view'][] = HOME . "view/" . str_replace(['.php', '.html'], '', $item);
-
-        } else {
-
-            foreach (Helper::listFolder(PATH_HOME . VENDOR) as $lib) {
-                foreach (Helper::listFolder(PATH_HOME . VENDOR) as $libOverload) {
-                    if ($libOverload !== $lib) {
-                        if (file_exists(PATH_HOME . VENDOR . $libOverload . "/public/overload/{$lib}/view/{$item}.php") || file_exists(PATH_HOME . VENDOR . $libOverload . "/public/overload/{$lib}/view/{$item}.html")) {
-                            $findItem = !0;
-                            break;
-                        }
-                    }
-                }
-
-                if (file_exists(PATH_HOME . VENDOR . $lib . "/public/view/{$setor}/{$item}.php") || file_exists(PATH_HOME . VENDOR . $lib . "/public/view/{$setor}/{$item}.html") || file_exists(PATH_HOME . VENDOR . $lib . "/public/view/{$item}.php") || file_exists(PATH_HOME . VENDOR . $lib . "/public/view/{$item}.html")) {
-                    $data['data']['view'][] = HOME . "view/" . str_replace(['.php', '.html'], '', $item);
-                    break;
-                }
-            }
-        }
-
-        if($findItem)
-            $data['data']['view'][] = HOME . "view/" . str_replace(['.php', '.html'], '', $item);
-    }
-}*/
+$views = (file_exists(PATH_HOME . "_config/offline/{$setor}/view.json") ? json_decode(file_get_contents(PATH_HOME . "_config/offline/{$setor}/view.json"), !0) : []);
+foreach ($views as $view)
+    $data['data'][] = ['view' => $view, 'js' => (file_exists(PATH_HOME . "assetsPublic/view/{$setor}/{$view}.min.js") ? "assetsPublic/view/{$setor}/{$view}.min.js" : "assetsPublic/view/{$view}.min.js")];
