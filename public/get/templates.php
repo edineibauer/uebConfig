0103a3bdf67c43892fe4ec4391963b6033784857
Edinei J. Bauer <galera.org@gmail.com>
2020-05-13 12:09:01 -0300

    templates default

--- public/get/templates.php
+++ public/get/templates.php
@@ -1,6 +1,13 @@
 <?php
 
-$data['data'] = ["header", "aside", "menuHeader", "note", "notificacoesEmpty", "installAppCard"];
+$data['data'] = [];
+$data['data']["header"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/header.mustache");
+$data['data']["aside"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/aside.mustache");
+$data['data']["menuHeader"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/menuHeader.mustache");
+$data['data']["note"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/note.mustache");
+$data['data']["notificacoesEmpty"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/notificacoesEmpty.mustache");
+$data['data']["installAppCard"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/installAppCard.mustache");
+
 foreach (\Config\Config::getRoutesFilesTo("param", "json") as $fileDir) {
     $p = \Config\Config::getJsonFile($fileDir);
 
