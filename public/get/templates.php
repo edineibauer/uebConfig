e131010184feb59c9c3b3d65a823870a9ca4e1c4
Edinei J. Bauer <galera.org@gmail.com>
2020-05-14 22:22:58 -0300

    pega templates do setor automático, e prioriza overload dos templates defaults

--- public/get/templates.php
+++ public/get/templates.php
@@ -1,12 +1,13 @@
 <?php
 
 $data['data'] = [];
-$data['data']["header"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/header.mustache");
-$data['data']["aside"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/aside.mustache");
-$data['data']["menuHeader"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/menuHeader.mustache");
-$data['data']["note"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/note.mustache");
-$data['data']["notificacoesEmpty"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/notificacoesEmpty.mustache");
-$data['data']["installAppCard"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/installAppCard.mustache");
+$setor = \Config\Config::getSetor();
+
+foreach (\Config\Config::getRoutesFilesTo("tpl", "mustache") as $tpl => $tplDir) {
+    $tplName = str_replace('.mustache', '', $tpl);
+    if (!isset($data['data'][$tplName]) && preg_match("/\/tpl\/{$setor}\//i", $tplDir))
+        $data['data'][$tplName] = file_get_contents($tplDir);
+}
 
 foreach (\Config\Config::getRoutesFilesTo("param", "json") as $fileDir) {
     $p = \Config\Config::getJsonFile($fileDir);
@@ -23,3 +24,21 @@ foreach (\Config\Config::getRoutesFilesTo("param", "json") as $fileDir) {
         }
     }
 }
+
+if (!isset($data['data']['header']))
+    $data['data']["header"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/header.mustache");
+
+if (!isset($data['data']['aside']))
+    $data['data']["aside"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/aside.mustache");
+
+if (!isset($data['data']['menuHeader']))
+    $data['data']["menuHeader"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/menuHeader.mustache");
+
+if (!isset($data['data']['note']))
+    $data['data']["note"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/note.mustache");
+
+if (!isset($data['data']['notificacoesEmpty']))
+    $data['data']["notificacoesEmpty"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/notificacoesEmpty.mustache");
+
+if (!isset($data['data']['installAppCard']))
+    $data['data']["installAppCard"] = file_get_contents(PATH_HOME . VENDOR . "config/public/tpl/installAppCard.mustache");
