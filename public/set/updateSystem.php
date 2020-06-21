35be0d8a5e2b1c451f76d863c79e0d5e5d278a4c
Edinei J. Bauer <galera.org@gmail.com>
2020-06-21 15:54:12 -0300

    updateSystem update the class before

--- public/set/updateSystem.php
+++ public/set/updateSystem.php
@@ -3,7 +3,14 @@ $pass = \Helpers\Check::password(filter_input(INPUT_POST, 'pass', FILTER_DEFAULT
 
 $read = new \Conn\Read();
 $read->exeRead("usuarios", "WHERE nome = 'Admin' AND status = 1 AND password = '{$pass}'");
-if($read->getResult())
+if($read->getResult()) {
+
+    /**
+     * Update the class UpdateSystem, then execute
+     */
+    unlink(PATH_HOME . VENDOR . "config/public/src/Config/UpdateSystem.php");
+    copy(PATH_HOME . "vendor/ueb/config/public/src/Config/UpdateSystem.php", PATH_HOME . VENDOR . "config/public/src/Config/UpdateSystem.php");
     new \Config\UpdateSystem();
+}
 
 $data['data'] = $read->getResult()? !0 : !1;
--- public/view/updateSystem/updateSystem.php
+++ public/view/updateSystem/updateSystem.php
@@ -1,11 +1,14 @@
 <script>
-    if(senha = prompt("Senha:")) {
-        post("config", "updateSystem", {pass: senha}, function (g) {
-            if(g) {
-                location.href = "<?=HOME?>";
-            } else {
-                toast("Senha inválida", 2000, "toast-warning");
-            }
-        })
+    if(navigator.onLine) {
+        if (senha = prompt("Senha:")) {
+            toast("Atualizando Sistema...", 100000);
+            post("config", "updateSystem", {pass: senha}, function (g) {
+                if (g) {
+                    location.href = "<?=HOME?>";
+                } else {
+                    toast("Senha inválida", 2000, "toast-warning");
+                }
+            })
+        }
     }
 </script>
