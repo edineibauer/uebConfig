4e59777d7793b2dcec911e9f1d79b4a3bb81d0ca
Edinei J. Bauer <galera.org@gmail.com>
2020-07-01 10:52:09 -0300

    add function setUserData on front

--- public/set/setUserData.php
+++ public/set/setUserData.php
@@ -3,3 +3,15 @@
 $dados = filter_input(INPUT_POST, 'data', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
 $dados['id'] = $_SESSION['userlogin']['setorData']['id'];
 $data['data'] = \Entity\Entity::update($_SESSION['userlogin']['setor'], $dados);
+
+/**
+ * Salva perfil do usuário em last user profile in front to not update front user
+ */
+if(is_int($data['data'])) {
+    foreach ($dados as $column => $value)
+        $_SESSION['userlogin']['setorData'][$column] = $value;
+
+    $f = fopen(PATH_HOME . "_cdn/userPerfil/" . $_SESSION['userlogin']['id'] . ".json", "w+");
+    fwrite($f, json_encode($_SESSION['userlogin']));
+    fclose($f);
+}
