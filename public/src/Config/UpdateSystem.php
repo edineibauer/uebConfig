0b51551df1ac085be61ab60d82ef9d8c1844f80b
Edinei J. Bauer <galera.org@gmail.com>
2020-07-06 10:04:46 -0300

    remove reportTable from coreGrid

--- public/src/Config/UpdateSystem.php
+++ public/src/Config/UpdateSystem.php
@@ -359,7 +359,6 @@ class UpdateSystem
              */
             $m = new \MatthiasMullie\Minify\JS(file_get_contents(PATH_HOME . VENDOR . "config/public/assets/libs/table.js"));
             $m->add(file_get_contents(PATH_HOME . VENDOR . "config/public/assets/libs/grid.js"));
-            $m->add(file_get_contents(PATH_HOME . VENDOR . "config/public/assets/libs/reportTable.js"));
             $m->add(file_get_contents(PATH_HOME . VENDOR . "config/public/assets/libs/pagination.js"));
             $f = fopen(PATH_HOME . VENDOR . "config/public/assets/coreGrid.js", "w");
             fwrite($f, trim(preg_replace('/(?:(?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:(?<!\:|\\\|\'|\")\/\/.*))/', '', $m->minify())));
