890f3db586083e876c80c1ccb977b1d16ee9f1b4
Edinei J. Bauer <galera.org@gmail.com>
2020-07-07 07:55:39 -0300

    fix setUserData

--- public/set/setUserData.php
+++ public/set/setUserData.php
@@ -1,7 +1,12 @@
 <?php
 
-$dados = filter_input(INPUT_POST, 'data', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+$dados = filter_input_array(INPUT_POST, FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+unset($dados['maestruToken'], $dados['fileInSetFolder']);
 $dados['id'] = $_SESSION['userlogin']['setorData']['id'];
+
+/**
+ * Update user data
+ */
 $data['data'] = \Entity\Entity::update($_SESSION['userlogin']['setor'], $dados);
 
 /**
