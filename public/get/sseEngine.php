fd462046af76c66d8f7efef8ee5485c69b7c18aa
Edinei J. Bauer <galera.org@gmail.com>
2020-07-28 16:51:02 -0300

    SSE

--- public/get/sseEngine.php
+++ public/get/sseEngine.php
@@ -6,68 +6,75 @@ header('Cache-Control: no-cache');
 $view = "";
 $messages = [];
 if (!empty($_SESSION['userlogin'])) {
+    $view = file_get_contents(PATH_HOME . "_cdn/userLastView/" . $_SESSION['userlogin']['id'] . ".txt");
 
     \Helpers\Helper::createFolderIfNoExist(PATH_HOME . "_cdn/userSSE/{$_SESSION['userlogin']['id']}");
 
-    function writeUserSSE(string $event, array $message) {
+    /**
+     * @param string $event
+     * @param string $message
+     */
+    function writeUserSSE(string $event, string $message)
+    {
         $f = fopen(PATH_HOME . "_cdn/userSSE/{$_SESSION['userlogin']['id']}/{$event}.json", "w");
-        fwrite($f, json_encode($message));
+        fwrite($f, $message);
         fclose($f);
     }
 
-    function returnSSE(string $view, $messages)
+    /**
+     * @param string $view
+     * @param array $messages
+     */
+    function returnSSE(string $view, array $messages)
     {
         echo "id: " . time() . PHP_EOL;
         echo "retry: 1000" . PHP_EOL;
         $content = [];
         foreach ($messages as $event => $message) {
-            if(!file_exists(PATH_HOME . "_cdn/userSSE/{$_SESSION['userlogin']['id']}/{$event}.json")) {
-                writeUserSSE($event, $message);
+            if (!file_exists(PATH_HOME . "_cdn/userSSE/{$_SESSION['userlogin']['id']}/{$event}.json")) {
+                writeUserSSE($event, json_encode($message));
                 $content[$event] = $message;
             } else {
                 $f = file_get_contents(PATH_HOME . "_cdn/userSSE/{$_SESSION['userlogin']['id']}/{$event}.json");
-                if($f !== json_encode($message)) {
-                    writeUserSSE($event, $message);
+                $messageString = json_encode($message);
+                if ($f !== $messageString) {
+                    writeUserSSE($event, $messageString);
                     $content[$event] = $message;
                 }
             }
         }
 
-        if(!empty($content))
+        if (!empty($content))
             echo "event: " . $view . PHP_EOL . "data: " . json_encode($content) . PHP_EOL . PHP_EOL;
 
         ob_flush();
         flush();
     }
 
-    if(!empty($_SESSION['userlogin']['id'])) {
-        $view = file_get_contents(PATH_HOME . "_cdn/userLastView/" . $_SESSION['userlogin']['id'] . ".txt");
+    foreach (\Config\Config::getRoutesFilesTo("view/{$view}/sse", "php") as $route) {
+        $data = null;
 
-        foreach (\Config\Config::getRoutesFilesTo("view/{$view}/sse", "php") as $route) {
-            $data = null;
+        ob_start();
 
-            ob_start();
-
-            try {
-                include_once $route;
-                if (!empty($data['error'])) {
-                    $data["response"] = 2;
-                    $data["data"] = "";
-                } elseif (!isset($data['data'])) {
-                    $data = ["response" => 1, "error" => "", "data" => ob_get_contents()];
-                } elseif (!isset($data['response'])) {
-                    $data['response'] = 1;
-                    $data['error'] = "";
-                }
-
-            } catch (Exception $e) {
-                $data = ["response" => 2, "error" => "Erro na resposta do Servidor", "data" => ""];
+        try {
+            include_once $route;
+            if (!empty($data['error'])) {
+                $data["response"] = 2;
+                $data["data"] = "";
+            } elseif (!isset($data['data'])) {
+                $data = ["response" => 1, "error" => "", "data" => ob_get_contents()];
+            } elseif (!isset($data['response'])) {
+                $data['response'] = 1;
+                $data['error'] = "";
             }
 
-            ob_end_clean();
-
-            $messages[pathinfo($route, PATHINFO_FILENAME)] = $data;
+        } catch (Exception $e) {
+            $data = ["response" => 2, "error" => "Erro na resposta do Servidor", "data" => ""];
         }
+
+        ob_end_clean();
+
+        $messages[pathinfo($route, PATHINFO_FILENAME)] = $data;
     }
 }
 
--- public/get/sseEngineClear.php
+++ public/get/sseEngineClear.php
@@ -2,5 +2,5 @@
 
 if(!empty($_SESSION['userlogin']['id'])) {
     foreach (\Helpers\Helper::listFolder(PATH_HOME . "_cdn/userSSE/{$_SESSION['userlogin']['id']}") as $item)
-        unlink(PATH_HOME . "_cdn/userSSE/{$_SESSION['userlogin']['id']}/{$item}.json");
+        unlink(PATH_HOME . "_cdn/userSSE/{$_SESSION['userlogin']['id']}/{$item}");
 }
