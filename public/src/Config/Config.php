dee366fa07d45ecefab030c9decf514790a34724
Edinei J. Bauer <galera.org@gmail.com>
2020-07-31 15:54:41 -0300

    getpermission fix the permission select

--- public/src/Config/Config.php
+++ public/src/Config/Config.php
@@ -268,7 +268,7 @@ class Config
         if ($setor === "admin")
             return !0;
 
-        $permissoes = self::getPermission($setor);
+        $permissoes = self::getPermission($setor)[$setor];
         if (empty($options))
             return isset($permissoes[$entity]);
 
