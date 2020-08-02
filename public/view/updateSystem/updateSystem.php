f742e4414544f540235d185e3c154def02f75a62
Edinei J. Bauer <galera.org@gmail.com>
2020-08-02 11:33:24 -0300

    toast atualizando sistema bitter time

--- public/view/updateSystem/updateSystem.php
+++ public/view/updateSystem/updateSystem.php
@@ -2,16 +2,19 @@
     if(navigator.onLine) {
         if (senha = prompt("Senha:")) {
             toast("Atualizando Sistema...", 1000000000);
-            post("config", "updateSystem", {pass: senha}, function (g) {
-                if (g) {
+            AJAX.post("updateSystem", {pass: senha}).then(g => {
+                if(g) {
+                    if(typeof sseSource !== "undefined" && navigator.onLine && typeof (EventSource) !== "undefined")
+                        sseSource.close();
+
                     localStorage.removeItem('update');
                     checkUpdate().then(() => {
-                        location.href = HOME;
+                        location.href = HOME + "dashboard";
                     })
                 } else {
                     toast("Senha inválida", 2000, "toast-warning");
                 }
-            })
+            });
         }
     }
 </script>
