4e1fd724aaf94f086fc3e7ed50501880816a4cb7
Edinei J. Bauer <galera.org@gmail.com>
2020-08-02 11:47:42 -0300

    admin post not need password

--- public/post/admin/updateSystem.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Update the class UpdateSystem, then execute
- */
-unlink(PATH_HOME . VENDOR . "config/public/src/Config/UpdateSystem.php");
-copy(PATH_HOME . "vendor/ueb/config/public/src/Config/UpdateSystem.php", PATH_HOME . VENDOR . "config/public/src/Config/UpdateSystem.php");
-new \Config\UpdateSystem();
-
-$data['data'] = !0;
--- public/post/updateSystem.php
+++ public/post/updateSystem.php
@@ -1,16 +1,19 @@
 <?php
-$pass = \Helpers\Check::password(filter_input(INPUT_POST, 'pass', FILTER_DEFAULT));
 
-$read = new \Conn\Read();
-$read->exeRead("usuarios", "WHERE setor IS NULL && status = 1 && password = '{$pass}'");
-if($read->getResult()) {
+$data['data'] = $_SESSION['userlogin']['setor'] === "admin";
 
-    /**
-     * Update the class UpdateSystem, then execute
-     */
+if(!$data['data']) {
+    $pass = \Helpers\Check::password(filter_input(INPUT_POST, 'pass', FILTER_DEFAULT));
+    $read = new \Conn\Read();
+    $read->exeRead("usuarios", "WHERE setor IS NULL && status = 1 && password = '{$pass}'");
+    $data['data'] = $read->getResult() ? !0 : !1;
+}
+
+/**
+ * Update the class UpdateSystem, then execute
+ */
+if($data['data']) {
     unlink(PATH_HOME . VENDOR . "config/public/src/Config/UpdateSystem.php");
     copy(PATH_HOME . "vendor/ueb/config/public/src/Config/UpdateSystem.php", PATH_HOME . VENDOR . "config/public/src/Config/UpdateSystem.php");
     new \Config\UpdateSystem();
 }
-
-$data['data'] = $read->getResult()? !0 : !1;
