092a1a9f355ed6be8742078ed681e8992d857ec7
Edinei J. Bauer <galera.org@gmail.com>
2020-08-12 09:30:23 -0300

    update notification use of usuario

--- public/sse/notificationsBadge.php
+++ public/sse/notificationsBadge.php
@@ -2,6 +2,6 @@
 
 if(!empty($_SESSION['userlogin']) && $_SESSION['userlogin']['id'] > 0) {
     $sql = new \Conn\SqlCommand();
-    $sql->exeCommand("SELECT count(id) as total FROM " . PRE . "notifications_report WHERE usuario = {$_SESSION['userlogin']['id']} && (recebeu IS NULL || recebeu = 0)");
+    $sql->exeCommand("SELECT count(id) as total FROM " . PRE . "notifications_report WHERE ownerpub = {$_SESSION['userlogin']['id']} && (recebeu IS NULL || recebeu = 0)");
     $data['data'] = $sql->getResult()[0]['total'];
 }
