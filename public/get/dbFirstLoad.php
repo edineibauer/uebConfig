1668067d3f55031a38074386051d208c2950686a
Edinei J. Bauer <galera.org@gmail.com>
2020-09-01 17:37:35 -0300

    carrega database with the app first load

--- public/get/dbFirstLoad.php
+++ public/get/dbFirstLoad.php
@@ -1,12 +1,9 @@
 <?php
-/**
- * Check if have new data
- * if have, send it to front
- */
+
 $data['data'] = [];
 
 /**
- * First clear the user DB last historic ID, so
+ * Clear the user DB last historic ID, so
  * now we can get the new content
  */
 foreach (\Helpers\Helper::listFolder(PATH_HOME . "_cdn/userSSE/{$_SESSION['userlogin']['id']}") as $item)
@@ -24,32 +21,15 @@ foreach (\Helpers\Helper::listFolder(PATH_HOME . "entity/cache") as $item) {
          * If the user have permissions to read the data entity
          */
         if (\Config\Config::haveEntityPermission($entity, ["read"])) {
+            $data['data'][$entity] = \Entity\Entity::exeRead($entity);
 
-            /**
-             * Get last historic ID
-             */
             $json = new \Entity\Json();
             $hist = $json->get("historic");
 
             /**
-             * If not have historic ID, so
-             * create a new One
-             */
-            if (empty($hist[$entity])) {
-                $hist[$entity] = strtotime('now') . "-" . rand(1000000, 9999999);
-                $json->save("historic", $hist);
-            }
-
-            /**
-             * Check if have new data
-             * if have, send it to front
-             */
-            $data['data'][$entity] = \Entity\Entity::exeRead($entity);
-
-            /**
              * save info that says to front that the data entity is send to front right now
              */
-            $f = fopen(PATH_HOME . "_cdn/userSSE/{$_SESSION['userlogin']['id']}/{$entity}.json", "w");
+            $f = fopen(PATH_HOME . "_cdn/userSSE/{$_SESSION['userlogin']['id']}/db_{$entity}.json", "w");
             fwrite($f, $hist[$entity]);
             fclose($f);
         }
