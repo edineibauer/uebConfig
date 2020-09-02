9346d2aa90361d8ef1c49491a0229adbd38e31f5
Edinei J. Bauer <galera.org@gmail.com>
2020-09-02 08:24:49 -0300

    setuserdata update from after

--- public/post/setUserData.php
+++ public/post/setUserData.php
@@ -8,15 +8,3 @@ $dados['id'] = $_SESSION['userlogin']['setorData']['id'];
  * Update user data
  */
 $data['data'] = \Entity\Entity::update($_SESSION['userlogin']['setor'], $dados);
-
-/**
- * Salva perfil do usuário em last user profile in front to not update front user
- */
-if(is_int($data['data'])) {
-    foreach ($dados as $column => $value)
-        $_SESSION['userlogin']['setorData'][$column] = $value;
-
-    $f = fopen(PATH_HOME . "_cdn/userPerfil/" . $_SESSION['userlogin']['id'] . ".json", "w+");
-    fwrite($f, json_encode($_SESSION['userlogin']));
-    fclose($f);
-}
