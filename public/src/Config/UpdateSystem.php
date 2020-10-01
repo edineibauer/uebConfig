8bc4b82c9c4f2edce6955e2b3eba92e294fa47be
Edinei J. Bauer <galera.org@gmail.com>
2020-10-01 17:22:53 -0300

    clear cache db when update system

--- public/src/Config/UpdateSystem.php
+++ public/src/Config/UpdateSystem.php
@@ -180,7 +180,32 @@ class UpdateSystem
             }
         }
 
+        $this->createCachedDB();
+
         $this->result = true;
+    }
+
+    private function createCachedDB()
+    {
+        $sql = new SqlCommand();
+
+        /**
+         * Para cada entidade no sistema, cria o cache
+         */
+        foreach (Helper::listFolder(PATH_HOME . "entity/cache") as $entity) {
+            if(pathinfo($entity, PATHINFO_EXTENSION) === "json") {
+                $entity = str_replace(".json", "", $entity);
+
+                /**
+                 * Delete cached database
+                 * Create cached database if not exist
+                 */
+                $sql->exeCommand(
+                    "DROP TABLE IF EXISTS " . PRE . "wcache_" . $entity . ";"
+                    . "CREATE TABLE IF NOT EXISTS `" . PRE . "wcache_" . $entity . "` (`id` INT(11) NOT NULL, `system_id` INT(11) DEFAULT NULL, `data` longtext DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+                    . "ALTER TABLE `" . PRE . "wcache_" . $entity . "` ADD PRIMARY KEY (`id`), MODIFY `id` int(11) NOT NULL AUTO_INCREMENT");
+            }
+        }
     }
 
     private function deleteInstall()
