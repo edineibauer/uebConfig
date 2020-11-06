1826941adaa32b382257ebf83dd9e9243e094249
Edinei J. Bauer <edineibauer@gmail.com>
2020-11-06 09:46:57 -0300

    update button shadow

--- public/assets/libs/appCore.js
+++ public/assets/libs/appCore.js
@@ -1060,7 +1060,7 @@ async function checkUpdate() {
         if (!localStorage.update)
             localStorage.update = await AJAX.post("update");
         else if (parseFloat(VERSION) > parseFloat(localStorage.update))
-            toast("<div class='left'>Nova versão</div><button style='float: right;border: none;outline: none;box-shadow: none;padding: 10px 20px;border-radius: 5px;margin: -5px -11px -5px 20px;background: #fff;color: #555;cursor: pointer;' onclick='updateCache()'>atualizar</button>", 15000, "toast-success");
+            toast("<div class='left'>Nova versão</div><button style='float: right;border: none;outline: none;box-shadow: none;padding: 10px 20px;border-radius: 5px;margin: -5px -11px -5px 20px;background: #fff;color: #555;cursor: pointer;box-shadow: 0px 2px 5px -4px black' onclick='updateCache()'>atualizar</button>", 15000, "toast-success");
     }
 }
 
