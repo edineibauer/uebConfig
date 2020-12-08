411c0a927ab434f981e0d69770741f1cedd4d9cb
Edinei J. Bauer <edineibauer@gmail.com>
2020-12-08 09:52:23 -0300

    push Notification com cordova

--- public/assets/libs/appCore.js
+++ public/assets/libs/appCore.js
@@ -3432,6 +3432,113 @@ function onHistoryBack(funcao) {
 }
 
 /**
+ * Push Notification Functions
+ * @param asyncFunc
+ * @param onSuccess
+ * @param onFailure
+ * @param customTries
+ */
+function trySomeTimes(asyncFunc, onSuccess, onFailure, customTries) {
+    var tries = typeof customTries === "undefined" ? 100 : customTries;
+    var interval = setTimeout(function () {
+        if (typeof asyncFunc !== "function") {
+            onSuccess("Unavailable");
+            return;
+        }
+        asyncFunc()
+            .then(function (result) {
+                if ((result !== null && result !== "") || tries < 0) {
+                    onSuccess(result);
+                } else {
+                    trySomeTimes(asyncFunc, onSuccess, onFailure, tries - 1);
+                }
+            })
+            .catch(function (e) {
+                clearInterval(interval);
+                onFailure(e);
+            });
+    }, 100);
+}
+
+function setupOnTokenRefresh() {
+    FCM.eventTarget.addEventListener(
+        "tokenRefresh",
+        function (data) {
+            AJAX.post("pushNotificationRegister", {tokenPush: data.detail, code: "FCM"});
+        },
+        false
+    );
+}
+
+function setupOnNotification() {
+    FCM.eventTarget.addEventListener(
+        "notification",
+        function (data) {
+            console.log(data.detail);
+        },
+        false
+    );
+    FCM.getInitialPushPayload()
+        .then((payload) => {
+            console.log("Initial Payload ", payload);
+        })
+        .catch((error) => {
+            console.log("Initial Payload Error ", error);
+        });
+}
+
+function registerFCMToken() {
+    console.log('FCM');
+    trySomeTimes(
+        FCM.getToken,
+        function (token) {
+            console.log(token);
+            AJAX.post("pushNotificationRegister", {tokenPush: token, code: "FCM"});
+        },
+        function (error) {
+            console.log("Error on listening for FCM token: " + error);
+        }
+    );
+}
+
+function registerAPNSToken() {
+    if (cordova.platformId !== "ios")
+        return;
+
+    FCM.getAPNSToken(
+        function (token) {
+            AJAX.post("pushNotificationRegister", {tokenPush: token, code: "APNS"});
+        },
+        function (error) {
+            console.log("Error on listening for APNS token: " + error);
+        }
+    );
+}
+
+function waitForPermission(callback) {
+    FCM.requestPushPermission()
+        .then(function (didIt) {
+            if (didIt) {
+                callback();
+            } else {
+                console.log("Push permission was not given to this application");
+            }
+        })
+        .catch(function (error) {
+            console.log("Error on checking permission: " + error);
+        });
+}
+
+function setupListeners() {
+    waitForPermission(function () {
+        registerFCMToken();
+        registerAPNSToken();
+        setupOnTokenRefresh();
+        setupOnNotification();
+    });
+}
+
+/**
  * Ao carregar todo o documento executa esta função
  */
 async function onLoadDocument() {
@@ -3461,6 +3568,8 @@ async function onLoadDocument() {
 
         AJAX.get("isOffline");
     };
+
+    document.addEventListener("deviceready", setupListeners, false);
 
     /**
      * Intercepta clicks em links e traduz na função "pageTransition()"
