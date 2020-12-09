fb34288a3f0f3976f0a33251e19adfd47034ca4b
Edinei J. Bauer <edineibauer@gmail.com>
2020-12-09 08:57:59 -0300

    move notifications to Notifications lib

--- public/sse/notificationsBadge.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if(!empty($_SESSION['userlogin']) && $_SESSION['userlogin']['id'] > 0) {
-    $sql = new \Conn\SqlCommand();
-    $sql->exeCommand("SELECT count(id) as total FROM " . PRE . "notifications_report WHERE ownerpub = {$_SESSION['userlogin']['id']} && (recebeu IS NULL || recebeu = 0)");
-    $data['data'] = $sql->getResult()[0]['total'];
-}
