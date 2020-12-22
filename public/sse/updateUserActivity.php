689ab1ce4aa37361fae08724fcadec103540fdc9
Edinei J. Bauer <edineibauer@gmail.com>
2020-12-22 09:00:57 -0300

    set is online file into userActivity

--- public/sse/updateUserActivity.php
+++ public/sse/updateUserActivity.php
@@ -3,27 +3,29 @@
 $data['data'] = "";
 if (!empty($_SESSION['userlogin']) && !empty($_SESSION['userlogin']['token'])) {
 
+    if(!file_exists(PATH_HOME . "_cdn/userActivity/" . $_SESSION['userlogin']['id'] . "/isOnline.json"))
+        \Config\Config::createFile(PATH_HOME . "_cdn/userActivity/" . $_SESSION['userlogin']['id'] . "/isOnline.json", "");
+
     /**
      * Update the time when this user is online in the app
      */
     $dia = date("Y-m-d");
-    if (!file_exists(PATH_HOME . "_cdn/userActivity/" . $_SESSION['userlogin']['id'] . "/{$dia}.json")) {
+    if (file_exists(PATH_HOME . "_cdn/userActivity/" . $_SESSION['userlogin']['id'] . "/{$dia}.json")) {
+
+        /**
+         * Set the time now
+         */
+        $diaContent = json_decode(file_get_contents(PATH_HOME . "_cdn/userActivity/" . $_SESSION['userlogin']['id'] . "/{$dia}.json"), !0);
+        $diaContent[] = date("H:i:s");
+        \Config\Config::createFile(PATH_HOME . "_cdn/userActivity/" . $_SESSION['userlogin']['id'] . "/{$dia}.json", json_encode($diaContent));
+
+    } else {
+
+        /**
+         * Cretae the file day
+         */
+        \Helpers\Helper::createFolderIfNoExist(PATH_HOME . "_cdn/userActivity");
         \Helpers\Helper::createFolderIfNoExist(PATH_HOME . "_cdn/userActivity/" . $_SESSION['userlogin']['id']);
-        $f = fopen(PATH_HOME . "_cdn/userActivity/" . $_SESSION['userlogin']['id'] . "/{$dia}.json", "w+");
-        fwrite($f, "[]");
-        fclose($f);
+        \Config\Config::createFile(PATH_HOME . "_cdn/userActivity/" . $_SESSION['userlogin']['id'] . "/{$dia}.json", json_encode([date("H:i:s")]));
     }
-
-    /**
-     * Set the time now
-     */
-    $diaContent = json_decode(file_get_contents(PATH_HOME . "_cdn/userActivity/" . $_SESSION['userlogin']['id'] . "/{$dia}.json"), !0);
-    $diaContent[] = date("H:i:s");
-
-    /**
-     * Save the file
-     */
-    $f = fopen(PATH_HOME . "_cdn/userActivity/" . $_SESSION['userlogin']['id'] . "/{$dia}.json", "w+");
-    fwrite($f, json_encode($diaContent));
-    fclose($f);
 }
