ab13d76256112dfc19e0a1eb9de9f58b8f6e6f9d
Edinei J. Bauer <edineibauer@gmail.com>
2020-12-29 10:48:27 -0300

    config view fixed

--- public/view/UIConfiguracoes/config.js
+++ public/view/UIConfiguracoes/config.js
@@ -1,33 +1,15 @@
-function hide_sidebar_small() {
-    if (screen.width < 993) {
-        $("#myOverlay, #mySidebar").css("display", "none")
-    }
-}
-
-function mainLoading() {
-    hide_sidebar_small();
-    closeSidebar()
-}
-
 async function menuDashboard() {
     let allow = await dbLocal.exeRead("__allow", 1);
     let info = await dbLocal.exeRead("__info", 1);
-    let templates = await getTemplates();
     let menu = [];
     let indice = 0;
 
     for(let entity in dicionarios) {
-        if (typeof info[entity] !== "undefined" && info[entity]['user'] === 3 && typeof allow !== "undefined" && typeof allow[entity] !== "undefined" && typeof allow[entity].menu !== "undefined" && allow[entity].menu) {
+        if (typeof info[entity] !== "undefined" && info[entity]['user'] === 3 && typeof allow !== "undefined" && typeof allow[entity] !== "undefined" && typeof allow[entity].menu !== "undefined" && allow[entity].menu && !menu.find(e => {return e.entity === entity})) {
             menu.push({
                 indice: indice++,
                 icon: (info[entity].icon !== "" ? info[entity].icon : "storage"),
                 title: ucFirst(replaceAll(replaceAll(entity, "_", " "), "-", " ")),
-                table: !0,
-                link: !1,
-                form: !1,
-                page: !1,
-                file: '',
-                lib: '',
                 entity: entity
             });
         }
@@ -35,38 +17,25 @@ async function menuDashboard() {
 
     menu.sort(dynamicSort('indice'));
 
-    let tpl = (menu.length < 4 ? templates.menuCard : templates.menuLi);
-    $("#config-menu").html("");
-    for(let m of menu)
-        $("#config-menu").append(Mustache.render(tpl, m));
+    let $menu = $("#mySidebar");
+    $menu.html("");
+    for(let m of menu) {
+        let result = await db.exeRead(m.entity);
+        $menu.append("<a href='form-maestru/" + m.entity + (!isEmpty(result) ? "/" + result[0].id : "") + "' data-target='#config-panel'><i class='material-icons left'>" + m.icon + "</i><span class='left padding-tiny padding-left'>" + m.title + "</span></a>");
+        if(isEmpty(result)) {
+            /**
+             * track change on menu ID
+             */
+            let li = setInterval(async function () {
+                if(typeof form !== "undefined" && typeof form.id !== "undefined" && form.id !== "") {
+                    clearInterval(li);
+                    menuDashboard();
+                }
+            }, 50);
+        }
+    }
 }
 
 $(function () {
     menuDashboard();
-
-    $("body").off("click", ".menu-li:not(.not-menu-li)").on("click", ".menu-li:not(.not-menu-li)", function () {
-        if ($(this).hasAttr("data-action")) {
-            let action = $(this).attr("data-action");
-            checkUpdate();
-            mainLoading();
-
-            if (action === "table") {
-                (async () => {
-                    let entity = $(this).data("entity");
-                    let result = await db.exeRead(entity, null, 1);
-                    let id = (!isEmpty(result) ? result[0].id : null);
-                    pageTransition(entity, 'form', 'forward', "#dashboard", {id: id});
-                })();
-
-            } else if (action === 'form') {
-                // let fields = (typeof $(this).attr("data-fields") !== "undefined" ? JSON.parse($(this).attr("data-fields")) : "undefined");
-                let id = !isNaN($(this).attr("data-atributo")) && $(this).attr("data-atributo") > 0 ? parseInt($(this).attr("data-atributo")) : null;
-                pageTransition($(this).data("entity"), 'form', 'forward', "#dashboard", id);
-
-            } else if (action === 'page') {
-
-                pageTransition($(this).attr("data-atributo"), 'route', 'forward', "#core-content");
-            }
-        }
-    });
 })
