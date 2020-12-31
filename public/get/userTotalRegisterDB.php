40823a6c7ae0acb02a102018cf71044691384346
Edinei J. Bauer <edineibauer@gmail.com>
2020-12-31 09:50:06 -0300

    default total register logic

--- public/get/userTotalRegisterDB.php
+++ public/get/userTotalRegisterDB.php
@@ -1,14 +1,31 @@
 <?php
 
+use Config\Config;
 use Helpers\Helper;
 
+$list = [];
 $data['data'] = [];
+$setor = Config::getSetor();
+$permissoes = Config::getPermission($setor);
+
+//read all dicionarios
 foreach (Helper::listFolder(PATH_HOME . "entity/cache") as $entity) {
-    $entidade = str_replace(".json", "", $entity);
-    if(preg_match("/.json$/i", $entity) && \Config\Config::haveEntityPermissionRead($entidade)) {
-        $read = new \Conn\Read();
-        $read->setSelect("COUNT(id) as total");
-        $read->exeRead($entidade);
-        $data['data'][$entidade] = $read->getResult() ? $read->getResult()[0]['total'] : 0;
+    if ($entity !== "info" && substr($entity, -4, 4) === "json") {
+
+        $entidade = substr($entity, 0, -5);
+
+        /**
+         * If is admin, or if not have permissions information, or if have read permission to my setor
+         */
+        if ($setor === "admin" || empty($permissoes[$entidade]) || (isset($permissoes[$entidade]['read']) && $permissoes[$entidade]['read']))
+            $list[] = PRE . $entidade;
     }
+}
+
+$sql = new \Conn\SqlCommand();
+$sql->exeCommand("SELECT TABLE_NAME, TABLE_ROWS FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '" . DATABASE . "' AND TABLE_NAME IN ('" . implode("', '", $list) . "')");
+if($sql->getResult()) {
+    $tt = strlen(PRE);
+    foreach ($sql->getResult() as $item)
+        $data['data'][substr($item['TABLE_NAME'], $tt)] = (int) $item['TABLE_ROWS'];
 }
