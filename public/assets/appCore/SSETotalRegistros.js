48c4a7521774edb04817f6901439277f8e0693bc
Edinei J. Bauer <edineibauer@gmail.com>
2021-01-01 19:38:35 -0300

    sse grid

--- public/assets/appCore/SSETotalRegistros.js
+++ public/assets/appCore/SSETotalRegistros.js
@@ -3,7 +3,7 @@ $(function () {
         if (!isEmpty(data)) {
             await dbLocal.exeCreate("__totalRegisters", data);
 
-            if(typeof grids !== "undefined")
+            if(typeof grids !== "undefined" && typeof grids === "object" && !isEmpty(grids))
                 Object.values(grids)[0].updateTotalTable();
         }
     });
