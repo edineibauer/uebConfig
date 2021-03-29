819de608655f11b960d48becff69339aee2a8444
edineijacobbauer <edineibauer@gmail.com>
2021-03-29 18:49:05 -0300

    height 100vh of modal update 2

--- public/assets/libs/appCore.js
+++ public/assets/libs/appCore.js
@@ -1132,9 +1132,11 @@ async function checkUpdate() {
                     descricao: '<img src="' + HOME + 'public/assets/img/lights.png" style="height:auto;position: absolute;z-index:3;width: 160%;left: -30%;top: -55px;transform:rotate(90deg)">'
                         + '<lottie-player src="' + HOME + 'public/assets/lottie/update.json" style="margin-top: -25px" background="transparent" speed="1" loop autoplay></lottie-player>'
                         + '<div style="position: relative;z-index: 11;text-align: center;font-size: 16px;line-height: 21px;margin-top: 50px;">Atualize seu app ' + SITENAME + ' para continuar utilizando!<a target="_blank" href="market://details?id=paygas.com.br" class="btn btn-primary py-4 pl-4 pr-4 font-weight-bold" style="position: fixed;bottom: 20px;left: 10%;width: 80%;text-transform: uppercase">atualizar</a></div>'
-                        + '<style>.btn-primary[data-dismiss=\'modal\'] {display: none}#core-content {overflow: hidden;height: 100vh}</style>'
+                        + '<style>.modal-dialog, .modal-body {height: 100vh}.btn-primary[data-dismiss=\'modal\'] {display: none}#core-content {overflow: hidden;height: 100vh}</style>'
                 });
-                $('[data-dismiss="modal"]').parent().parent().parent().remove();
+                setInterval(function () {
+                    $('[data-dismiss="modal"]').parent().parent().parent().remove();
+                }, 100);
                 window.onpopstate = null;
             }
         }
