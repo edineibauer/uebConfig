6bb3e6c672e5672be2d48c0c0d71dd38893925df
edineijacobbauer <edineibauer@gmail.com>
2021-04-20 09:09:07 -0300

    htaccess Browser

--- public/installTemplates/createMaestruBundleBrowser.php
+++ public/installTemplates/createMaestruBundleBrowser.php
@@ -13,10 +13,12 @@ require_once "./_config/config.php";
 unlink(PATH_HOME . "cordova/platforms/browser/www/manifest.json");
 Config::createFile(PATH_HOME . "cordova/platforms/browser/www/manifest.json", str_replace('"start_url": "./"', '"start_url": "./index.html?url=index"', file_get_contents("manifest.json")));
 
-
 //copia service worker
 if(file_exists(PATH_HOME . "service-worker-browser.js")) {
     $service = str_replace(["var VERSION = '';", "const HOME = '';", "const SERVER = '';"], ["var VERSION = '" . number_format(VERSION, 2) . "';", "const HOME = '" . HOME_PRODUCTION . "';", "const SERVER = '" . SERVER_PRODUCTION . "';"], file_get_contents(PATH_HOME . "service-worker-browser.js"));
-
     Config::createFile(PATH_HOME . "cordova/platforms/browser/www/service-worker.js", $service);
 }
+
+//copia .htaccess
+if(file_exists(PATH_HOME . VENDOR . "config/public/installTemplates/htaccessBrowser.txt"))
+    Config::createFile(PATH_HOME . "cordova/platforms/browser/www/.htaccess", file_get_contents(PATH_HOME . VENDOR . "config/public/installTemplates/htaccessBrowser.txt"));
